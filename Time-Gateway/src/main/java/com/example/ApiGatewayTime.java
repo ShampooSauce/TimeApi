aa94e3d84ceaa1ebea02b4de2035f8d6789817d2
Houssein <69593077+ShampooSauce@users.noreply.github.com>
2023-03-21 17:23:50 +0700

    add the GateWay for TimeApi

---
+++ Time-Gateway/src/main/java/com/example/ApiGatewayTime.java
@@ -0,0 +1,10 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayTime {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayTime.class, args);
+	}
+}
